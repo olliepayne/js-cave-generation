5cccc26f51bc6381aec6337fce627fe41b618103
olliepayne <olliepayne@me.com>
2021-02-19 20:24:06 -0700

    add cave logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const board = {
  width: 5,
  height: 5,
- fillPercent: 0.3,
+ fillPercent: 0.4,
  generations: 3,
  virtualGrid: [],
  domGrid: [],
@@ -23,12 +23,12 @@ const board = {
   for(let i = 0; i < this.generations; i++) {
    for(let x = 0; x < this.width; x++) {
     for(let y = 0; y < this.height; y++) {
-     const neighbors = this.checkNeighbors({ x, y })
-
-     if(this.virtualGrid[x][y] === 1) {
-      if(neighbors < 2) this.virtualGrid[x][y] = 0
-     } else if(this.virtualGrid[x][y] === 0) {
-      if(neighbors > 4) this.virtualGrid[x][y] = 1
+     if(x > 0 && x < this.width - 1 && y > 0 && y < this.height - 1) {
+      if(this.virtualGrid[x][y] === 1) {
+       const newCoords = this.checkNeighbors({ x, y })
+       this.virtualGrid[x][y] = 0
+       this.virtualGrid[newCoords.x][newCoords.y] = 1
+      }
      }
     }
    }
@@ -38,21 +38,23 @@ const board = {
   }
  },
  checkNeighbors(coords) {
-  let neighborCount = 0
+  let newCoords = coords
 
   for(let neighborX = coords.x - 1; neighborX <= coords.x + 1; neighborX++) {
    for(let neighborY = coords.y - 1; neighborY <= coords.y  + 1; neighborY++) {
     if(neighborX === coords.x && neighborY === coords.y) {
      continue
-    } else if(neighborX < 0 || neighborX === this.width || neighborY < 0 || neighborY === this.height) {
-     continue
-    } else if(this.virtualGrid[neighborX][neighborY] === 1) {
-     neighborCount++
+    }
+
+    if(this.virtualGrid[neighborX][neighborY] === 0) {
+     newCoords.x = neighborX
+     newCoords.y = neighborY
+     return newCoords
     }
    }
   }
 
-  return neighborCount
+  return newCoords
  }
 }
 
